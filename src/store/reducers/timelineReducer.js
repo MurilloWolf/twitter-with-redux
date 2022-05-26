9a5ce117ae7fd1caa6cdd3ca892e98748758f549
MurilloWolf <mwolfcavalheiro@gmail.com>
2022-05-26 10:45:41 -0300

    Maked redux setup

---
+++ src/store/reducers/timelineReducer.js
@@ -0,0 +1,25 @@
+import { TWEETAR, DELETE_TWEET } from '../actions/actionTypes';
+
+const initialState = {
+  tweets: [],
+};
+
+const timelineReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case TWEETAR:
+      return {
+        tweets: [
+          ...state.tweets,
+          { name: action.payload.name, message: action.payload.message },
+        ],
+      };
+
+    case DELETE_TWEET:
+      return {};
+
+    default:
+      return state;
+  }
+};
+
+export default timelineReducer;
